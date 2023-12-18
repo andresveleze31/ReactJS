f9d5b3f8f34c72269e9157c6e06773f5059b8c0e
andresveleze31 <142637448+andresveleze31@users.noreply.github.com>
2023-12-18 17:55:23 -0500

    Finishing

--- quiosco-app/src/context/QuiscoProvider.jsx
+++ quiosco-app/src/context/QuiscoProvider.jsx
@@ -10,6 +10,28 @@ function QuioscoProvider({ children }) {
   const [modal, setModal] = useState(false);
   const [pedidos, setPedidos] = useState([]);
   const [added, setAdded] = useState(false);
+  const [nombre, setNombre] = useState("");
+  const [total, setTotal] = useState(0);
+
+  async function colocarOrden(e) {
+    e.preventDefault();
+
+    try {
+      const { data } = await axios.post("/api/ordenes", {
+        pedido: pedidos,
+        nombre,
+        total,
+        fecha: Date.now().toString(),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    setCategoriaActual(categorias[0]);
+    setProducto({});
+    setNombre("");
+    setPedidos([]);
+  }
 
   function agregarPedido(pedido) {
     if (pedidos.some((p) => p.producto.id === pedido.producto.id)) {
@@ -26,14 +48,27 @@ function QuioscoProvider({ children }) {
     }
   }
 
-  function eliminarPedido(pedido){
+  useEffect(() => {
+    calcularTotal();
+  }, [pedidos]);
+
+  function calcularTotal() {
+    let totalCalc = pedidos.reduce((acumulador, pedido) => {
+      console.log("Hola", pedido.producto.precio, pedido.cantidad);
+      return acumulador + pedido.producto.precio * pedido.cantidad;
+    }, 0);
+
+    setTotal(totalCalc);
+  }
+
+  function eliminarPedido(pedido) {
     if (pedidos.some((p) => p.producto.id === pedido.producto.id)) {
-      const nuevoPedido = pedidos.filter((p) => {        
-        return p.producto.id!== pedido.producto.id
+      const nuevoPedido = pedidos.filter((p) => {
+        return p.producto.id !== pedido.producto.id;
       });
 
       setPedidos(nuevoPedido);
-    } 
+    }
   }
 
   async function obtenerCategorias() {
@@ -79,6 +114,10 @@ function QuioscoProvider({ children }) {
         setAdded,
         pedidos,
         eliminarPedido,
+        nombre,
+        setNombre,
+        total,
+        colocarOrden,
       }}
     >
       {children}
--- quiosco-app/src/pages/total.jsx
+++ quiosco-app/src/pages/total.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "@/layout/layout";
 import { useQuiosco } from "@/hooks/useQuiosco";
 
 function Total() {
+  const { nombre, setNombre, pedidos, total, colocarOrden } = useQuiosco();
+  const [habilitado, setHabilitado] = useState(true);
+
+  useEffect(() => {
+    if (pedidos.length <= 0 || nombre === "") {
+      setHabilitado(true);
+    } else {
+      setHabilitado(false);
+    }
+  }, [nombre, pedidos]);
+
   return (
     <Layout>
       <h1 className="font-black">Total</h1>
+      <p>Confirma tu Pedido a continuacion</p>
+
+      <form onSubmit={(e) => colocarOrden(e)} action="">
+        <div>
+          <label
+            className="block mt-[2rem] uppercase font-bold text-gray-500 "
+            htmlFor="nombre"
+          >
+            Nombre
+          </label>
+          <input
+            className="mt-[1rem] p-[1rem] w-1/3 border border-black"
+            type="text"
+            placeholder="Ingresa tu Nombre"
+            onChange={(e) => setNombre(e.target.value)}
+            value={nombre}
+          />
+        </div>
+
+        <p className="text-[1.6rem] mt-[1rem]">
+          Total a pagar: <span className="font-black">${total}</span>
+        </p>
+
+        <input
+          className={
+            habilitado
+              ? "text-white font-bold uppercase text-center py-[1rem] w-1/3 bg-indigo-200 bg-opacity-90 mt-[1rem] "
+              : "text-white font-bold uppercase text-center py-[1rem] w-1/3 bg-indigo-500 hover:bg-opacity-90 mt-[1rem] hover:cursor-pointer "
+          }
+          type="submit"
+          value="Confirmar pedido"
+          disabled={habilitado}
+        />
+      </form>
     </Layout>
   );
 }
